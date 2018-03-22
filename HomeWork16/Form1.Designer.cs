28a771be105ddf327bc4d2f5bb6bcc3ff73ec09b
Agreen1216 <35696793+Agreen1216@users.noreply.github.com>
2018-03-22 03:03:31 -0400

    Add files via upload

---
+++ HomeWork16/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace HomeWork16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.String = new System.Windows.Forms.Label();
+            this.String1 = new System.Windows.Forms.Label();
+            this.Results = new System.Windows.Forms.Label();
+            this.PathValue = new System.Windows.Forms.TextBox();
+            this.TextString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.ResultSearch = new System.Windows.Forms.ListBox();
+            this.ExtValue1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // String
+            // 
+            this.String.AutoSize = true;
+            this.String.Location = new System.Drawing.Point(29, 40);
+            this.String.Name = "String";
+            this.String.Size = new System.Drawing.Size(25, 13);
+            this.String.TabIndex = 1;
+            this.String.Text = "Ext ";
+            // 
+            // String1
+            // 
+            this.String1.AutoSize = true;
+            this.String1.Location = new System.Drawing.Point(29, 66);
+            this.String1.Name = "String1";
+            this.String1.Size = new System.Drawing.Size(34, 13);
+            this.String1.TabIndex = 2;
+            this.String1.Text = "String";
+            this.String1.Click += new System.EventHandler(this.String1_Click);
+            // 
+            // Results
+            // 
+            this.Results.AutoSize = true;
+            this.Results.Location = new System.Drawing.Point(29, 95);
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(42, 13);
+            this.Results.TabIndex = 3;
+            this.Results.Text = "Results";
+            this.Results.Click += new System.EventHandler(this.Results_Click);
+            // 
+            // PathValue
+            // 
+            this.PathValue.Location = new System.Drawing.Point(71, 6);
+            this.PathValue.Name = "PathValue";
+            this.PathValue.Size = new System.Drawing.Size(119, 20);
+            this.PathValue.TabIndex = 4;
+            this.PathValue.TextChanged += new System.EventHandler(this.PathValue_TextChanged);
+            // 
+            // TextString
+            // 
+            this.TextString.Location = new System.Drawing.Point(70, 58);
+            this.TextString.Name = "TextString";
+            this.TextString.Size = new System.Drawing.Size(122, 20);
+            this.TextString.TabIndex = 6;
+            this.TextString.TextChanged += new System.EventHandler(this.TextString_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path ";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(-5, 189);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 8;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(159, 189);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 9;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // ResultSearch
+            // 
+            this.ResultSearch.FormattingEnabled = true;
+            this.ResultSearch.Location = new System.Drawing.Point(70, 95);
+            this.ResultSearch.Name = "ResultSearch";
+            this.ResultSearch.Size = new System.Drawing.Size(120, 95);
+            this.ResultSearch.TabIndex = 10;
+            this.ResultSearch.SelectedIndexChanged += new System.EventHandler(this.ResultSearch_SelectedIndexChanged);
+            // 
+            // ExtValue1
+            // 
+            this.ExtValue1.Location = new System.Drawing.Point(71, 32);
+            this.ExtValue1.Name = "ExtValue1";
+            this.ExtValue1.Size = new System.Drawing.Size(119, 20);
+            this.ExtValue1.TabIndex = 11;
+            this.ExtValue1.TextChanged += new System.EventHandler(this.ExtValue1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 261);
+            this.Controls.Add(this.ExtValue1);
+            this.Controls.Add(this.ResultSearch);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextString);
+            this.Controls.Add(this.PathValue);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.String1);
+            this.Controls.Add(this.String);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label String;
+        private System.Windows.Forms.Label String1;
+        private System.Windows.Forms.Label Results;
+        private System.Windows.Forms.TextBox PathValue;
+        private System.Windows.Forms.TextBox TextString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.ListBox ResultSearch;
+        private System.Windows.Forms.TextBox ExtValue1;
+    }
+}
+
---
+++ HomeWork16/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HomeWork16
+{
+    public partial class Form1 : Form
+    {
+        FileStream fs;
+        StreamWriter sw;
+        int ErrorCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void String1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            ErrorCount = 0;
+            if (PathValue.Text == "")
+            {
+                PathValue.Text = "Please Enter a folder path to Search";
+                ErrorCount = ErrorCount + 1;
+            }
+            else
+            if (ExtValue1.Text == "")
+            {
+                ExtValue1.Text = " Please enter correct field";
+                ErrorCount = ErrorCount + 1;
+
+                String1.Text = "";
+                TextString.Text = "";
+                ExtValue1.Text = "";
+            }
+            try
+            {
+                fs = new FileStream("Log.txt", FileMode.Append, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.WriteLine("Data Time: " + DateTime.Now + ": " + "Start Process");
+
+                // populate listbox with files matching user input.
+                PopulateListBox(ResultSearch, PathValue.Text, ExtValue1.Text, String1.Text);
+
+                sw.WriteLine("Data Time: " + DateTime.Now + ": " + "End Process");
+                sw.Close();
+            }
+            catch (IOException ex)
+            {
+                sw.WriteLine("Data Time: " + DateTime.Now + ": " + "IO Exception Thrown: " + ex.Message);
+                sw.Close();
+                return;
+            }
+            catch (Exception ex)
+            {
+                sw.WriteLine("Data Time: " + DateTime.Now + ": " + "Exception Thrown: " + ex.Message);
+                sw.Close();
+                return;
+            }
+        
+          
+
+            //string userFilePathInput;
+            //string userExtensionInput;
+            //string[] files;
+            //// assigning global variables equal to text and combo box values
+            //userFilePathInput = PathValue.Text; // text box value for file path
+            //userExtensionInput = ExtValue1.Text; // combo box value for extension
+
+            //// getting files per user input as arguements
+            //files = Directory.GetFiles(userFilePathInput, "*" + userExtensionInput);
+
+
+
+        }
+
+        // retrieve files within a directory (path) matching the extension given by the user.
+        private void PopulateListBox(ListBox ResultValue, string PathValue, string ExtValue1, string String1)
+        {
+            // The path value given by the user should be an existing directory.
+            // If not, DirectoryNotFoundException will be thrown.
+
+            try
+            {
+                //Set the current directory to the user's path value.
+                Directory.SetCurrentDirectory(this.PathValue.Text);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                sw.WriteLine("Data Time: " + DateTime.Now + ": " + "Exception Thrown: Directory Path Not Found");
+
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            // get files from the current directory with the extension specified by the user.
+            // for each file, search its contents for the search string specified by the user.
+            // when this string is found, add the file name to the listbox.
+            DirectoryInfo directinfo = new DirectoryInfo(PathValue);
+            FileInfo[] files = directinfo.GetFiles("*" + ExtValue1);
+            foreach (FileInfo file in files)
+            {
+                bool stringfound = false;
+                stringfound = SearchFile(file, String1);
+                if (stringfound)
+                {
+                    ResultValue.Items.Add(file.Name);
+                }
+            }
+        }
+
+        private bool SearchFile(FileInfo file, string stringvalue)
+        {
+            bool stringfound = false;
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(file.ToString()))
+                {
+                    string line;
+                    sr.Read();
+                    // Read and check lines for search string from the file
+                    // until a match is found, or the end of file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.ToLower().Contains(stringvalue.ToLower()))
+                        {
+                            stringfound = true;
+                            break;
+                        }
+                        else
+                        {
+                            stringfound = false;
+                        }
+                        sr.Read();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                sw.WriteLine("Data Time: " + DateTime.Now + ": " + "Exception Thrown: File Not Read");
+
+                throw;
+
+            }
+            return stringfound;
+        }
+
+        private void searchFile(string fileName, string searchString)
+        {
+            //    /*
+            //open file
+            //    Read one line at a time
+            //    Examine each line to see if it contains searchString
+            //    if it contains the string 
+            //       return true 
+            //    */
+
+
+            
+           
+        }
+
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            PathValue.Text = "";
+            String1.Text = "";
+            TextString.Text = "";
+            ExtValue1.Text = "";
+            ResultSearch.Items.Clear();
+        }
+
+
+        private void Results_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void TextString_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PathValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExtValue_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExtValue1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
